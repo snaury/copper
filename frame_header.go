a72bf5daea618cdbe00ee4e21a515000f2a2b432
Alexey Borzenkov <snaury@yandex-team.ru>
2015-06-25 21:13:11 +0300

    Add a Makefile for making it easier to run test coverage

--- frame_header.go
+++ frame_header.go
@@ -11,14 +11,11 @@ type frameHeader struct {
 }
 
 func readFrameHeader(r io.Reader) (hdr frameHeader, err error) {
-	var n int
 	var buf [8]byte
-	n, err = io.ReadFull(r, buf[0:8])
+	_, err = io.ReadFull(r, buf[0:8])
 	if err == nil {
 		hdr.streamID = binary.LittleEndian.Uint32(buf[0:4])
 		hdr.flagsSize = binary.LittleEndian.Uint32(buf[4:8])
-	} else if n == 0 && err == io.ErrUnexpectedEOF {
-		err = io.EOF
 	}
 	return
 }
