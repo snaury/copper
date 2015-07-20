dd32bf159a8a03b2d027c155c117e1194b87bdbe
Alexey Borzenkov <snaury@gmail.com>
2015-07-20 10:38:22 +0300

    Move frame header types to frame.go

--- frame.go
+++ frame.go
@@ -1,11 +1,26 @@
 package copper
 
 import (
+	"bytes"
 	"encoding/binary"
 	"errors"
 	"fmt"
 	"io"
 	"time"
+)
+
+// Various protocol constants
+const (
+	MaxStreamID                 = 1<<31 - 1
+	MinWindowSize               = 1024
+	MaxWindowSize               = 1<<31 - 1
+	MaxFramePayloadSize         = 0xffffff
+	MaxDataFramePayloadSize     = MaxFramePayloadSize
+	MaxResetFrameMessageSize    = MaxFramePayloadSize - 4
+	MaxSettingsFramePayloadSize = 6 * 1024
+	DefaultStreamWindow         = 65536
+	DefaultConnectionWindow     = 1 << 20
+	DefaultInactivityTimeout    = 60 * time.Second
 )
 
 // SettingID identifies a setting
@@ -24,24 +39,120 @@ const (
 	SettingInactivityMilliseconds SettingID = 3
 )
 
+// FrameType is an 8-bit frame type code
+type FrameType uint8
+
+// Defined frame type codes
+const (
+	FramePing     FrameType = 0
+	FrameData     FrameType = 1
+	FrameReset    FrameType = 2
+	FrameWindow   FrameType = 3
+	FrameSettings FrameType = 4
+)
+
+var frameNames = map[FrameType]string{
+	FramePing:     "PING",
+	FrameData:     "DATA",
+	FrameReset:    "RESET",
+	FrameWindow:   "WINDOW",
+	FrameSettings: "SETTINGS",
+}
+
+func (t FrameType) String() string {
+	if name, ok := frameNames[t]; ok {
+		return name
+	}
+	return fmt.Sprintf("UNKNOWN_FRAME_%d", uint8(t))
+}
+
+// FrameFlags stores 8-bit frame flags
+type FrameFlags uint8
+
+// Defined frame flags
+const (
+	FlagPingAck FrameFlags = 1
+
+	FlagDataEOF  FrameFlags = 1
+	FlagDataOpen FrameFlags = 2
+	FlagDataAck  FrameFlags = 4
+
+	FlagResetRead  FrameFlags = 1
+	FlagResetWrite FrameFlags = 2
+
+	FlagSettingsAck FrameFlags = 1
+)
+
+// Has returns true if all flags from v are present in f
+func (f FrameFlags) Has(v FrameFlags) bool {
+	return (f & v) == v
+}
+
+var flagNames = map[FrameType]map[FrameFlags]string{
+	FramePing: {
+		FlagPingAck: "ACK",
+	},
+	FrameData: {
+		FlagDataEOF:  "EOF",
+		FlagDataOpen: "OPEN",
+		FlagDataAck:  "ACK",
+	},
+	FrameReset: {
+		FlagResetRead:  "READ",
+		FlagResetWrite: "WRITE",
+	},
+	FrameSettings: {
+		FlagSettingsAck: "ACK",
+	},
+}
+
+func (f FrameFlags) String(t FrameType) string {
+	if f == 0 {
+		return "0"
+	}
+	var buf bytes.Buffer
+	var unknown FrameFlags
+	count := 0
+	for flag := FrameFlags(1); flag != 0; flag <<= 1 {
+		if f&flag == 0 {
+			continue
+		}
+		name := flagNames[t][flag]
+		if len(name) != 0 {
+			if count != 0 {
+				buf.WriteByte('|')
+			}
+			count++
+			buf.WriteString(name)
+		} else {
+			unknown |= flag
+		}
+	}
+	if unknown != 0 {
+		if count != 0 {
+			buf.WriteByte('|')
+		}
+		fmt.Fprintf(&buf, "0x%02x", uint8(unknown))
+	}
+	return buf.String()
+}
+
+// FrameHeader is a 9 byte frame header
+type FrameHeader struct {
+	Type     FrameType
+	Flags    FrameFlags
+	Length   uint32
+	StreamID uint32
+}
+
+func (h FrameHeader) String() string {
+	return fmt.Sprintf("[%s flags=%s length=%d stream=%d]", h.Type, h.Flags.String(h.Type), h.Length, h.StreamID)
+}
+
 // Frame is used to identify frame types
 type Frame interface {
 	writeFrameTo(w *FrameWriter) error
 }
-
-// Various protocol constants
-const (
-	MaxStreamID                 = 1<<31 - 1
-	MinWindowSize               = 1024
-	MaxWindowSize               = 1<<31 - 1
-	MaxFramePayloadSize         = 0xffffff
-	MaxDataFramePayloadSize     = MaxFramePayloadSize
-	MaxResetFrameMessageSize    = MaxFramePayloadSize - 4
-	MaxSettingsFramePayloadSize = 6 * 1024
-	DefaultStreamWindow         = 65536
-	DefaultConnectionWindow     = 1 << 20
-	DefaultInactivityTimeout    = 60 * time.Second
-)
 
 // FrameReader reads copper frames
 type FrameReader struct {
