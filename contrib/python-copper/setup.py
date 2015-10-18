dc60c5c3bbac9745ea6401523a948e30a66f772b
Alexey Borzenkov <snaury@gmail.com>
2015-10-19 01:45:32 +0300

    Make it possible to install python-copper sdist

--- contrib/python-copper/setup.py
+++ contrib/python-copper/setup.py
@@ -5,16 +5,14 @@ import subprocess
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
-SRC_PROTOCOL = '../../protocol'
-
-# Generated code built with newer versions of protobuf does not work with older
-# versions of python-protobuf, so it's better to regenerate it every time.
-subprocess.check_call([
-    'protoc',
-    '--python_out=lib/copper',
-    '--proto_path=' + SRC_PROTOCOL,
-    os.path.join(SRC_PROTOCOL, 'copper.proto'),
-])
+if os.path.isfile('copper.proto'):
+    # Generated code built with newer versions of protobuf does not work with older
+    # versions of python-protobuf, so it's better to regenerate it every time.
+    subprocess.check_call([
+        'protoc',
+        '--python_out=lib/copper',
+        'copper.proto',
+    ])
 
 class PyTest(TestCommand):
     user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
